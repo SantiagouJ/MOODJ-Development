ff2253b55539dfe088ea58526c729ae7bede6646
SantiagouJ <santiagojuradorodriguez3@gmail.com>
2025-05-29 21:39:14 -0500

    feat: Comments likes styles

--- src/components/postComponents/CommentCard.ts
+++ src/components/postComponents/CommentCard.ts
@@ -351,7 +351,7 @@ class CommentCard extends HTMLElement {
             }
         }
         </style>
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"/>
 
 
         
